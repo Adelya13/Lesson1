7f5281c6e6e63ab36dd93325fcd527a9bc2da958
Adelya <valisheva2002@mail.ruclear>
2020-09-10 14:15:28 +0300

    The first work

---
+++ max sum.pas
@@ -0,0 +1,20 @@
+﻿// Максимальная пара, которая делится нв 241, 1-й элемент больше 2-ого
+Const x=241;
+var a,b,i,n,p1,p2,j,mx : integer;
+dl : array [0..x-1] of integer;
+begin
+  readln(n);
+  readln(a);
+  mx:=0;
+  for i:=0 to x-1 do
+    dl[i]:=0;
+  for i:=1 to n-1 do begin
+    dl[a mod x]:=max(a,dl[a mod x]);
+    readln(b);
+    j:=( x - b mod x) mod x;
+    if (dl[j]>b) and (dl[j]+b>mx) then
+      (p1,p2,mx):=(dl[j],b,dl[j]+b);
+    a:=b;
+  end;
+  writeln(p1,' ',p2)
+end.
